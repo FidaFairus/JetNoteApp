680295bac0cea359dd94637e315852ed0adbe1c0
FidaFairus <70806162+FidaFairus@users.noreply.github.com>
2023-05-26 12:39:37 +0530

    adding note Bug fixed

--- app/src/main/java/com/experion/notetheme/screen/NoteScreen.kt
+++ app/src/main/java/com/experion/notetheme/screen/NoteScreen.kt
@@ -1,6 +1,5 @@
 package com.experion.notetheme.screen
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -42,127 +41,112 @@ import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+
 fun NoteScreen(
     notes: List<Note>,
-    onAddNote: (Note) -> Unit,
-    onRemoveNote: (Note) -> Unit
-) {
+    onAddNote:(Note)->Unit,
+    onRemoveNote:(Note)->Unit
+
+){
     var title by remember {
         mutableStateOf("")
     }
     var description by remember {
         mutableStateOf("")
     }
-    var context = LocalContext.current
-    Column(modifier = Modifier.padding(6.dp)) {
-        TopAppBar(
-            title = {
-                Text(text = stringResource(id = R.string.app_name))
-            },
-            actions = {
-                Icon(
-                    imageVector = Icons.Rounded.Notifications,
-                    contentDescription = "Icon"
-                )
-            },
-            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color(0xFFDADFE3))
+    var context= LocalContext.current
 
 
-        )
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            NoteInputText(
-                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
-                text = title,
-                label = "Title",
-                onTextChange = {
-                    if (it.all { char ->
-                            char.isLetter() || char.isWhitespace()
-                        }) title = it
+  Column(modifier= Modifier
+      .padding(6.dp)
+      ){
+      TopAppBar(modifier = Modifier,
+          title = {
+          Text(text = stringResource(id = R.string.app_name))},
 
-                }
-            )
-            NoteInputText(
-                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
-                text = description,
-                label = "Add a Note",
-                onTextChange = {
-                    if (it.all { char ->
-                            char.isLetter() || char.isWhitespace()
-                        }) description = it
-                }
-            )
-            NoteButton(text = "Save",
-                onClick = {
-                    if (title.isNotEmpty() && description.isNotEmpty()) {
-                        //save/add to list
-                        onAddNote(
-                            Note(
-                                title = title,
-                                description = description
-                            )
-                        )
-                        Toast.makeText(
-                            context, "Note Added",
-                            Toast.LENGTH_SHORT
-                        ).show()
+      actions = {
+          Icon(imageVector = Icons.Rounded.Notifications, contentDescription ="Icon" )
+      },
+          colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color(0xFFDADFE3))
+      )
+    //content
+      Column(modifier = Modifier.fillMaxWidth(),
+      horizontalAlignment = Alignment.CenterHorizontally) {
+          NoteInputText(text =title , label ="Title" , onTextChange = {
+              if (it.all { char->char.isLetter() || char.isWhitespace() })
+                  title= it
+          },
+          modifier = Modifier.padding(top=9.dp,bottom=8.dp))
 
-                    }
-                })
-        }
-        Divider(
-            modifier = Modifier.padding(10.dp)
-        )
-        LazyColumn {
-            items(notes) { note ->
-                NoteRow(note = note,
-                    onNoteClicked = {
-                        onRemoveNote(note)
-                    }
-                )
 
-            }
-        }
-    }
-
+          NoteInputText(text =description , label ="Add a note" , onTextChange = {
+              if (it.all { char->char.isLetter() || char.isWhitespace() })
+                  description= it
+          },
+              modifier = Modifier.padding(top=9.dp,bottom=8.dp))
+          NoteButton(text = "Save",
+              onClick = {
+              if (title.isNotEmpty() && description.isNotEmpty()){
+                  onAddNote(Note(title=title,
+                      description = description))
+                  title=""
+                  description=""
+                  Toast.makeText(context,"Note Added",Toast.LENGTH_SHORT).show()
+                  }
+         },
+              modifier = Modifier.padding(top=9.dp,bottom=8.dp))
+      }
+      Divider(
+          modifier=Modifier.padding(10.dp)
+      )
+     LazyColumn(){
+         items(notes){
+             note->
+             NoteRow(note = note,
+                 onNoteClicked ={
+                     onRemoveNote(note)
+                 } )
+         }
+     }
+  }
 }
 
 @Composable
 fun NoteRow(
-    modifier: Modifier = Modifier,
-    note: Note,
-    onNoteClicked: (Note) -> Unit,
+    modifier: Modifier=Modifier,
+    note:Note,
+    onNoteClicked :(Note)-> Unit
 ) {
     Surface(
-        modifier = Modifier
+        modifier
             .padding(4.dp)
             .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
             .fillMaxWidth(),
-        color = Color(0xFF6D6977),
-        shadowElevation = 6.dp
+    color= Color(0xFFDFE6EB),
     ) {
-        Column(
-            modifier
-                .clickable { onNoteClicked(note) }
-                .padding(horizontal = 14.dp, vertical = 6.dp),
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(text = note.title, style = MaterialTheme.typography.headlineMedium)
-            Text(text = note.description, style = MaterialTheme.typography.headlineSmall)
-            Text(
-                text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
-                style = MaterialTheme.typography.labelSmall
-            )
-        }
-
+    Column(
+        modifier
+            .clickable {
+                onNoteClicked(note)
+            }
+            .padding(14.dp, 6.dp),
+        horizontalAlignment = Alignment.Start
+    ){
+    Text(text = note.title,
+    style = MaterialTheme.typography.titleMedium)
+    Text(text = note.description,
+    style = MaterialTheme.typography.bodyLarge)
+    Text(text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE,d MMM")),
+    style = MaterialTheme.typography.bodySmall)
+    }
+    }
     }
 
-}
 
+@ExperimentalComposeUiApi
 @Preview(showBackground = true)
 @Composable
-fun NotesScreenPreview() {
+fun NoteScreenPreview(){
     NoteScreen(notes = emptyList(), onAddNote = {}, onRemoveNote = {})
+
 }
